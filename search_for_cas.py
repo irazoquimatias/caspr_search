90cc9c0e66ba44db63643bdf0711e218edbccc24
Matias Irazoqui <irazoqui.matias@gmail.com>
2020-07-21 16:19:39 -0300

    fixed indit

--- search_for_cas.py
+++ search_for_cas.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 import os
 import argparse
-import Bio.SeqIO
+from Bio import SeqIO
 
 #import subprocess as sp
 #from termcolor import colored
@@ -20,31 +20,31 @@ import Bio.SeqIO
 
 ## Parse options
 def parse_option(parser):
-	parser.add_argument("-i", '--input', type=str, help='Archivo FASTA a analizar')
-	parser.add_argument("-o", '--output', type=str, help='Directorio de salida')
-
-	parser.add_argument("-v", '--verbose', action='store_true')
-
-	args = parser.parse_args()
-	return args
+    parser.add_argument("-i", '--input', type=str, help='Archivo FASTA a analizar')
+    parser.add_argument("-o", '--output', type=str, help='Directorio de salida')
+    
+    parser.add_argument("-v", '--verbose', action='store_true')
+    
+    args = parser.parse_args()
+    return args
 
 ## Look for spacers
 def look_for_spacers (seq, options):
-	run_vmatch2 (seq, options)
+    run_vmatch2 (seq, options)
 #	run_fuzznuc
 #	extract_spacers
 
 def run_vmatch2 (seq, options):
-        os.makedirs (f'/tmp/casper_search/current_contig/')
-        os.chdir(f'/tmp/casper_search/current_contig/')
-        SeqIO.write(seq, f'contig.fasta', "fasta")
-        seq_id = seq.id
-        cmd = f' mkvtree2 -dna -pl -lcp -suf -tis -ois -bwt -bck -sti1 -db contig.fasta'
-        exec(cmd, verbose=verbose)
-        cmd = f'vmatch2 -l 23 25 60 -s leftseq -evalue 0.001 -absolute -noevalue -noscore \
-              -noidentity -sort sd -best 10000 -selfun ~/Programas/CRISPRCasFinder/sel392v2.so \
-              contig.fasta > {options.out}/{seq_id}_vmatch2.txt'
-	exec(cmd, verbose=verbose)
+    os.makedirs (f'/tmp/casper_search/current_contig/')
+    os.chdir(f'/tmp/casper_search/current_contig/')
+    SeqIO.write(seq, f'contig.fasta', "fasta")
+    seq_id = seq.id
+    cmd = f' mkvtree2 -dna -pl -lcp -suf -tis -ois -bwt -bck -sti1 -db contig.fasta'
+    exec(cmd, verbose=verbose)
+    cmd = f'vmatch2 -l 23 25 60 -s leftseq -evalue 0.001 -absolute -noevalue -noscore \
+          -noidentity -sort sd -best 10000 -selfun ~/Programas/CRISPRCasFinder/sel392v2.so \
+          contig.fasta > {options.out}/{seq_id}_vmatch2.txt'
+    exec(cmd, verbose=verbose)
 
 #def run_fuzznuc ():
 
@@ -63,10 +63,10 @@ def run_vmatch2 (seq, options):
 
 ## Main
 def main():
-	parser = argparse.ArgumentParser(description='')
-	options = parse_option(parser)
-        os.makedirs(options.output)
-        os.makedirs (f'/tmp/casper_search')
-        with open(options.input, "rU") as in_fasta:
-            for seq in SeqIO.parse(in_fasta, "fasta"):
-                look_for_spacers(seq, options)
+    parser = argparse.ArgumentParser(description='')
+    options = parse_option(parser)
+    os.makedirs(options.output)
+    os.makedirs (f'/tmp/casper_search')
+    with open(options.input, "rU") as in_fasta:
+        for seq in SeqIO.parse(in_fasta, "fasta"):
+            look_for_spacers(seq, options)
