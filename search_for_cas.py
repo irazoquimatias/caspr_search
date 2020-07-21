de0cbda625d4011ead248aa8c627d3bd859f0956
Matias Irazoqui <irazoquimatias@gmail.com>
2020-07-21 18:52:30 -0300

    modified:   search_for_cas.py

--- search_for_cas.py
+++ search_for_cas.py
@@ -22,45 +22,51 @@ import Bio.SeqIO
 def parse_option(parser):
 	parser.add_argument("-i", '--input', type=str, help='Archivo FASTA a analizar')
 	parser.add_argument("-o", '--output', type=str, help='Directorio de salida')
-	
-	parser.add_argument("-p", '--primers', type=str, help='fasta con primers')
+
 	parser.add_argument("-v", '--verbose', action='store_true')
-	parser.add_argument("-r", '--reference', help="fasta reference", default="/ref/MN996528.fna")
-	parser.add_argument("-a", '--annotation', help="fasta reference", default="/app/snpEff/data/covid19/genes.gbk")
-	
+
 	args = parser.parse_args()
 	return args
 
 ## Look for spacers
 def look_for_spacers (seq, options):
-	run_vmatch2
-	run_fuzznuc
-	extract_spacers
+	run_vmatch2 (seq, options)
+#	run_fuzznuc
+#	extract_spacers
 
-def run_vmatch2 ():
-	cmd = f'vmatch2 ... {sample}.fasta"'
+def run_vmatch2 (seq, options):
+        os.makedirs (f'/tmp/casper_search/current_contig/')
+        os.chdir(f'/tmp/casper_search/current_contig/')
+        SeqIO.write(seq, f'contig.fasta', "fasta")
+        seq_id = seq.id
+        cmd = f' mkvtree2 -dna -pl -lcp -suf -tis -ois -bwt -bck -sti1 -db contig.fasta'
+        exec(cmd, verbose=verbose)
+        cmd = f'vmatch2 -l 23 25 60 -s leftseq -evalue 0.001 -absolute -noevalue -noscore \
+              -noidentity -sort sd -best 10000 -selfun ~/Programas/CRISPRCasFinder/sel392v2.so \
+              contig.fasta > {options.out}/{seq_id}_vmatch2.txt'
 	exec(cmd, verbose=verbose)
 
-def run_fuzznuc ():
+#def run_fuzznuc ():
 
-def extract_spacers ():
+#def extract_spacers ():
 
 ## Look for CAS
-def look_for_cas ():
-	run_hmmsearch
-	find_best_cas
+#def look_for_cas ():
+#	run_hmmsearch
+#	find_best_cas
 
-def run_hmmsearch ():
+#def run_hmmsearch ():
 
-def find_best_cas ():
+#def find_best_cas ():
 
 ## Wrap everything
-
 
 ## Main
 def main():
 	parser = argparse.ArgumentParser(description='')
 	options = parse_option(parser)
-	in_fasta = bpio.read(option.input, "fasta")
-	for seq in in_fasta:
-		look_for_spacers(seq, options)
+        os.makedirs(options.output)
+        os.makedirs (f'/tmp/casper_search')
+        with open(options.input, "rU") as in_fasta:
+            for seq in SeqIO.parse(in_fasta, "fasta"):
+                look_for_spacers(seq, options)
