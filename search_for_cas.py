f38830c99b7c18f39c43d19a2188c81ec7aa0bc9
Matias Irazoqui <irazoqui.matias@gmail.com>
2020-07-24 16:40:20 -0300

    spacer listo

--- search_for_cas.py
+++ search_for_cas.py
@@ -22,19 +22,18 @@ def parse_option(parser):
     return args
 
 ## Look for spacers
-def look_for_spacers (seq, options):
-    run_vmatch2 (seq, options)
-#	extract_spacers
+def look_for_spacers (record, options):
+    (positions, dr) = run_vmatch2 (record, options)
+    extract_spacers (record, positions, options)
 
-def run_vmatch2 (seq, options):
+def run_vmatch2 (record, options):
     work_dir = '/tmp/casper_search/current_contig/'
     verbose = options.verbose
     if not os.path.exists(work_dir):
         os.makedirs (work_dir)
     os.chdir(work_dir)
 
-    SeqIO.write(seq, 'contig.fasta', 'fasta')
-    seq_id = seq.id
+    SeqIO.write(record, 'contig.fasta', 'fasta')
 
     cmd = 'mkvtree2 -dna -pl -lcp -suf -tis -ois -bwt -bck -sti1 -db contig.fasta'
     subprocess.run(cmd.split(' '))
@@ -51,15 +50,14 @@ def run_vmatch2 (seq, options):
         l = l.rstrip()
         if re.match('^>', l) or not l:
             continue
-        (score, positions) = run_fuzznuc(seq, l, options)
+        (score, positions) = run_fuzznuc(l, options)
         if score > best_score:
             best_score = score
             best_positions = positions
             best_dr = l
-        print (seq.id, best_dr, best_score)
-    print (seq.id, best_dr, best_score)
+    return (best_positions, best_dr)
 
-def run_fuzznuc (seq, pattern, options):
+def run_fuzznuc (pattern, options):
     score = 0
     positions = []
     cmd = 'fuzznuc -sequence contig.fasta -pattern %s -pmismatch %s -outfile fuzznuc.txt' \
@@ -74,12 +72,32 @@ def run_fuzznuc (seq, pattern, options):
                 positions.append((columns[0],columns[1]))
     return (score, positions)
 
-#def extract_spacers ():
+def extract_spacers (record, positions, options):
+    outfasta = options.output + '/CRISPR.fasta'
+    spacers = ''
+    if os.path.isfile(outfasta):
+        fasta = open(outfasta, 'a')
+    else:
+        fasta = open(outfasta, 'w')
+    for i in range (0, len(positions)-2):
+        start = int(positions[i][1])+2 # +1 por el indice, +1 porque el spacer empieza despues del repeat
+        end = int(positions[i+1][0])
+        spacer = str(record.seq)[start:end]
+        spacers = spacers + '>' + record.id + '_' + str(start) + '_' + str(end)
+        spacers = spacers + '\n' + spacer + '\n'
+    fasta.write(spacers)
 
 ## Look for CAS
-#def look_for_cas ():
+def look_for_cas (record, options):
+    run_prodigal (record, options)
 #	run_hmmsearch
 #	find_best_cas
+
+def run_prodigal (record, options):
+    if os.path.isfile('contig.fasta'):
+        print("esta")
+    else:
+        print("no esta")
 
 #def run_hmmsearch ():
 
@@ -89,17 +107,25 @@ def run_fuzznuc (seq, pattern, options):
 
 ## Main
 def main():
+    #bool_dependencies = check_dependencies()
+    #if not bool_dependencies:
+        #install_dependencies()
     parser = argparse.ArgumentParser(description='')
     options = parse_option(parser)
     current_dir = os.getcwd()
-    if not os.path.exists(options.output):
-        os.makedirs(options.output)
+    if not re.match('^/', options.output):
+        options.output = current_dir + '/' + options.output
+    if os.path.exists(options.output):
+        print("ERROR: la carpeta de salida existe")
+        exit
+    os.makedirs(options.output)
     if not os.path.exists("/tmp/casper_search"):
         os.makedirs ("/tmp/casper_search")
     with open(options.input, "rU") as in_fasta:
-        for seq in SeqIO.parse(in_fasta, "fasta"):
-            look_for_spacers(seq, options)
-    #shutil.rmtree("/tmp/casper_search")
+        for record in SeqIO.parse(in_fasta, "fasta"):
+            look_for_spacers(record, options)
+            look_for_cas(record, options)
+    shutil.rmtree("/tmp/casper_search")
 
 if __name__ == "__main__":
     main()
